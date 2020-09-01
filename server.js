76152a1a56d386d433a4339b4bcec0fd9e3a234c
dhruvshaarmaa <61281854+dhruvshaarmaa@users.noreply.github.com>
2020-09-01 13:50:55 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const express=require("express");
+const app=express();
+process.env.PORT=4041;
+const port=process.env.PORT || 4040;
+
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+});
+
+app.listen(port,()=>{
+    console.log(`Server started on http://localhost:${port}`);
+});
